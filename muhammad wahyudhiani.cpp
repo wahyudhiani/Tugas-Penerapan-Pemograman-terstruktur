872ac89712153dc5442e89b0a616ec6141dc9996
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2018-01-03 18:20:38 +0700

    deklarasi variabel pakai array 1 demensi

--- muhammad wahyudhiani.cpp
+++ muhammad wahyudhiani.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main(){
+	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+	double a1c[5]={4,4,5,3,3};
+	double a2c[5]={3,3,4,2,3};
+	double a3c[5]={5,4,2,2,2};
+	
+		
 		
 }	
 
