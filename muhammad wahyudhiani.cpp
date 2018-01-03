346c86b1e7ad05a0a87a6fc4ea7b9f15d8239cef
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2018-01-03 18:22:29 +0700

    deklarasi variabel array 1 dimensi

--- muhammad wahyudhiani.cpp
+++ muhammad wahyudhiani.cpp
@@ -8,7 +8,15 @@ int main(){
 	double a2c[5]={3,3,4,2,3};
 	double a3c[5]={5,4,2,2,2};
 	
-		
+		double mc[5]={
+		max(a1c[0],max(a2c[0],a3c[0])),
+		max(a1c[1],max(a2c[1],a3c[1])),
+		max(a1c[2],max(a2c[2],a3c[2])),
+		max(a1c[3],max(a2c[3],a3c[3])),
+		max(a1c[4],max(a2c[4],a3c[4]))
+	};
+	
+
 		
 }	
 
