dc9de1c1348578a67b89985ad58117ba43e0b2f6
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2018-01-03 18:25:47 +0700

    menampilkan nilai matriks x dgn perulangan

--- muhammad wahyudhiani.cpp
+++ muhammad wahyudhiani.cpp
@@ -31,7 +31,25 @@ double Ra1c[5]={ (a1c[0]/mc[0]),(a1c[1]/mc[1]),(a1c[2]/mc[2]),(a1c[3]/mc[3]),(a1
 			  	  RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4],
 			 	  RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4],};
 			 	  
-			 	  	
+			 	  	cout << "MATRIKS X :"<<endl;
+	int i=0;
+	do{
+		cout<<a1c[i] << " | ";
+		i++;
+
+		}while(i<5);cout<< endl;
+	int j=0;
+	do{
+		cout<<a2c[j] << " | ";
+		j++;
+		}while(j<5);cout << endl;
+	int k=0;
+	do{
+		cout<<a3c[k] << " | ";
+		k++;
+		}while(k<5);cout << endl;
+		
+	
 		
 }	
 
